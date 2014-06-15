63399f1d8e2381cbb6a061ad1f286579157b9724
thejeshgn <i@thejeshgn.com>
2014-06-15 21:27:45 +0530

    complted scraper coding

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-reservoir
-=========
+# Reservoir Data Scraper #
 
-Reservoir Details
+Scrapes Reservoir data and saves into SQLITE. Can also export into CSV, JSON
+
+Ref: http://www.ksndmc.org/Reservoir_Details.aspx
+
+## Download ##
+
+Downlonad from the releases and or clone the git project.
+
+
+## Install Dependencies ##
+`
+pip install -r requirements
+`
+
+## To run ##
+
+
+#### default: download last weeks data ####
+
+`
+python scrape.py
+`
+
+
+### Export data as json and csv ####
+`
+datafreeze export.yaml
+`
+
+
+
+## Author ##
+
+Thejesh GN <i@thejeshgn.com>
+
+Fingerprint: C7D4 1911 9893 ADAF 27B0 FCAA BFFC 8DD3 C06D D6B0
+
+<table>
+  <tr>
+    <td><img src="http://www.gravatar.com/avatar/4545b2a84b0ae407abc97ad8f23cc28b?s=60"></td><td valign="middle">Thejesh GN<br><a href="http:/thejeshgn.com">http://thejeshgn.com</a></td>
+    <td>i-at-thejeshgn-com <br> GPG ID :  0xBFFC8DD3C06DD6B0</td>
+  </tr>
+</table>
+
+
+## License ##
+IMD Data Scraper - Scrapes IMD data and saves into SQLITE. Can also export into CSV, JSON
+Copyright (C) 2014  Thejesh GN <i@thejeshgn.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ scraper.py
@@ -0,0 +1,83 @@
+#couldnt get this thing to work
+#!/usr/bin/env python
+import sys
+import sqlite3 as lite
+import requests
+import time
+from BeautifulSoup import BeautifulSoup
+reservoirs = ['Alamatti','Bhadra','Ghataprabha','Harangi','Hemavathi','K.R.S','Kabini','Linganamakki','Malaprabha','Narayanapura','Supa','Tungabhadra','Varahi']
+#weeks = [1]
+year = 2011
+week = 1
+
+
+for week in range(1,53): 
+    for reservoir in reservoirs:
+        con = lite.connect('./database/reservoir.sqlite')
+        cur = con.cursor()
+        access_url = "http://www.ksndmc.org/Reservoir_Details.aspx"
+        request_session = requests.Session()
+        html_src = ""
+        user_agent = {'User-agent': 'Mozilla/5.0'}
+        html_get_src = request_session.get("http://www.ksndmc.org/Reservoir_Details.aspx",headers = user_agent)
+        page = BeautifulSoup(html_get_src.text)
+        __VIEWSTATE = page.find(id="__VIEWSTATE")["value"]
+        __PREVIOUSPAGE = page.find(id="__PREVIOUSPAGE")["value"]
+        __EVENTVALIDATION = page.find(id="__EVENTVALIDATION")["value"]
+        all_coockies = requests.utils.dict_from_cookiejar(html_get_src.cookies)
+        NET_SessionId = all_coockies['ASP.NET_SessionId']
+        cookie = {'ASP.NET_SessionId': NET_SessionId}
+
+        #print __VIEWSTATE
+        #print __PREVIOUSPAGE
+        #print __EVENTVALIDATION
+        #print NET_SessionId
+
+
+        payload = {
+            'ctl00$cpMainContent$DropDownList1':  str(reservoir),
+            'ctl00$cpMainContent$Year_list':str(year),
+            'ctl00$cpMainContent$weekList': str(week),
+            'ctl00$cpMainContent$Button1':'Get Details',
+            "__VIEWSTATE":__VIEWSTATE,
+             "__PREVIOUSPAGE":__PREVIOUSPAGE,
+             "__EVENTVALIDATION":__EVENTVALIDATION,
+             "__EVENTTARGET":" ",
+             "__EVENTARGUMENT":" "
+
+        }
+        user_agent = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36','Referer':'https://www.ksndmc.org/Reservoir_Details.aspx','Content-Type':'application/x-www-form-urlencoded','Origin':'https://www.ksndmc.org','Host':'www.ksndmc.org','Accept-Encoding':'gzip,deflate,sdch','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+        html_post_src = request_session.post("https://www.ksndmc.org/Reservoir_Details.aspx",data=payload,cookies=html_get_src.cookies,headers = user_agent)
+        soup = BeautifulSoup(html_post_src.content)
+        tables = soup.findAll(id="ctl00_cpMainContent_GridView1")
+        for k in range(0, len(tables[0].contents)):
+            if k <= 1:
+                continue
+            #ignore if not tr
+            columns = []
+            if getattr(tables[0].contents[k], 'name', None) == 'tr':
+                row = tables[0].contents[k]
+                for r in range(0, len(row.contents)):
+                    if getattr(row.contents[r], 'name', None) == 'td': 
+                        td_column = row.contents[r]
+                        #print td_column
+                        #print td_column
+                        if len(td_column.contents[0]) > 0:                       
+                            value = td_column.contents[0]
+                            columns.append(value)
+                        else:
+                            columns.append("")
+            else:
+                continue
+            try:
+                UNIQUE_KEY = str(reservoir)+"_"+str(year)+"_"+str(week)+str(columns[1])
+                insert_data = {"RESERVOIR":reservoir , "YEAR":str(year) , "WEEK_NO":str(week)  , "FLOW_DATE":columns[1] , "PRESENT_STORAGE_TMC":columns[2] , "RES_LEVEL_FT":columns[3] , "INFLOW_CUSECS":columns[4] , "OUTFLOW_CUECS":columns[5],"UNIQUE_KEY":UNIQUE_KEY }
+                print insert_data
+                cur.execute('INSERT INTO reservoir_details (RESERVOIR , YEAR , WEEK_NO  , FLOW_DATE , PRESENT_STORAGE_TMC , RES_LEVEL_FT , INFLOW_CUSECS , OUTFLOW_CUECS,UNIQUE_KEY) VALUES (:RESERVOIR , :YEAR , :WEEK_NO  , :FLOW_DATE , :PRESENT_STORAGE_TMC , :RES_LEVEL_FT , :INFLOW_CUSECS , :OUTFLOW_CUECS, :UNIQUE_KEY)', insert_data)
+                con.commit()
+            except lite.IntegrityError:
+                print 'Duplicate, couldnt add'
+            time.sleep(3) 
+        con.close()
+
+
